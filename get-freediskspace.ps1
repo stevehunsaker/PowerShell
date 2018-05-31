1078bb578599fa9a2ea271d6e9156709c1191329
DataCorey <39804035+DataCorey@users.noreply.github.com>
2018-05-31 08:32:20 -0700

    Add files via upload

---
+++ get-freediskspace.ps1
@@ -0,0 +1,10 @@
+﻿
+$Computer = "localhost"
+$disk = get-wmiobject Win32_LogicalDisk -computer $computer
+$disksize = "Disk Size (MB)"
+$freespace = "Free Space (MB)" 
+" {0,18:n} {1,17:n} " -f $disksize, $freespace
+ForEach ($disk in $disk)
+{
+" {0} {1,15:n} {2,15:n}" -f $disk.DeviceID, ($disk.Size/1000), $($disk.freespace/1000)
+}
